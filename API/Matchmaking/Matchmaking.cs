a97e52205180d06b13a36ae26e55733d8bd172ca
Toby Wilkinson <tangentspy@outlook.com>
2015-09-06 10:53:33 +1000

    Added foundation code

---
+++ API/Matchmaking/Matchmaking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatchmakingTypes;
+
+namespace Matchmaking
+{
+    public class MatchmakingAPI
+    {
+        private List<Server> GameServers = new List<Server>();
+        private List<Player> Players = new List<Player>();
+        public List<Server> getServers(OnlineConnection ServerListConnection, string GMOD)
+        {
+            return GameServers;
+        }
+    }
+}
---
+++ API/Matchmaking/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchmakingTypes
+{
+    public class OnlineConnection
+    {
+        public string userName;
+        public string passWord;
+        public string Host;
+        public int Port;
+    }
+
+    public class Server
+    {
+        public string GMOD;
+        public string Name;
+        public string Host;
+        public int Port;
+        public int playerCount;
+        public int playerCap;
+    }
+
+    public class Player
+    {
+        public string Name;
+        public string Nick;
+        public int Level;
+        public string IP;
+        public int Port;
+        public bool isHost;
+    }
+}
